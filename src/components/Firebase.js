7a5a7d40b11341324ced26356ee532d200969b13
jessica joseph <jessicajoseph2019@yahoo.com>
2022-09-08 04:47:37 +0100

    project setup

---
+++ src/components/Firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBbtl5VZKesEHbQhpF1h3AEF-XACHTGNdU",
+    authDomain: "notes-21a9f.firebaseapp.com",
+    projectId: "notes-21a9f",
+    storageBucket: "notes-21a9f.appspot.com",
+    messagingSenderId: "342199964035",
+    appId: "1:342199964035:web:531aab2c59d12f21bca6b6",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default getFirestore(app)
