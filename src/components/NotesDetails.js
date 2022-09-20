723445a148d8482b48f17c7b66ee003cd2491401
jessica joseph <jessicajoseph2019@yahoo.com>
2022-09-21 00:16:08 +0100

    added the firebase query for the note details page

--- src/components/NotesDetails.js
+++ src/components/NotesDetails.js
@@ -1,37 +1,51 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { useParams } from 'react-router-dom';
+import db from './Firebase'
+
 
 
 function NotesDetails() {
+  const [note, setNote] = useState([])
+  const { id } = useParams();
+  const userData = async () => {
+    const NoteRef = collection(db, "notes");
+    const q = query(NoteRef, where("notes", "==", id));
+    const querySnapshot = await getDocs(q);
+    const result = querySnapshot.docs.map((doc) => (doc.data()))
+    setNote(result)
+    console.log(result)
+  }
+
+
+  useEffect(() => {
+    userData()
+  }, [])
 
   return (
     <>
       <header>
         <section className="container mx-auto flex flex-wrap px-4 lg:px-52 py-5 flex-col md:flex-row items-center">
-          <Link to="/note" className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-            <span className="ml-3 text-2xl text-Pink">Notes Title Here</span>
-          </Link>
           <aside className="md:ml-auto flex flex-wrap items-center text-base justify-center">
             <button className="inline-flex items-center bg-white text-black border-2 border-black py-1 px-6 focus:outline-none rounded text-base mt-4 md:mt-0">Edit </button>
             <button className="inline-flex items-center bg-blue text-white border-0 py-1.5 px-6 focus:outline-none rounded text-base mt-4 lg:ml-4 ml-2 md:mt-0">Delete </button>
           </aside>
+          {note.map((note)=>{
+            return(
+            <div key ={note.id}>
+              <h2>{note.title}</h2>
+              <p>{note.body}</p>
+            </div>
+         ) })}
         </section>
       </header>
 
 
       <section className="text-gray-600 body-font">
-          <article className="container px-5 py-24 mx-auto">
-            <summary className="flex flex-col w-full text-left mb-12">
-              <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Notes Body Starts Here</p>
-              <br/>
-              <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Motion design is a discipline that applies graphic design principles to filmmaking and video production through use of animation and visual effects. Examples include films, videos, animated text, 
-              and web-based animations and apps. Motion design has evolved as a direct result of technology improvements.</p>
-              <br/>
-              <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Motion design is a discipline that applies graphic design principles to filmmaking and video production through use of animation and visual effects. Examples include films, videos, animated text, and web-based animations and apps. 
-                Motion design has evolved as a direct result of technology improvements.</p>
-            </summary>
+        <article className="container px-5 py-24 mx-auto">
+          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">{note.body}</p>
 
-          </article>
+        </article>
       </section>
 
     </>
